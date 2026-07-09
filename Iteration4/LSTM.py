backprop fix with the gradient check
TODO

Test classifier on an actual dataset and see performance

Iteration4/LSTM.py
@@ -122,47 +122,56 @@ class LSTMCell:
         #loss function to be used is Sparse Categorical Cross Entropy Loss
 
         #output gate derivative
-        DO=DH*self.tanh(cachedValues.cnew)
+        DO=DH*self.tanh(cachedValues.get('cnew'))
 
         #cekk state gradient loss
-        DCell=DHCell+DH*cachedValues.outG*(1-(self.tanh(cachedValues.cnew))**2)
+        DCell=DHCell+DH*cachedValues.get('outG')*(1-(self.tanh(cachedValues.get('cnew')))**2)
 
 
-        DI=DCell*cachedValues.candG
+        DI=DCell*cachedValues.get('candG')
 
-        DCG=DCell*cachedValues.inputG
+        DCG=DCell*cachedValues.get('inputG')
         
 
-        DF=DCell*cachedValues.cOld
+        DF=DCell*cachedValues.get('cOld')
 
+ 
+
+        DFGpre=DF*cachedValues.get('forgetG')*(1-cachedValues.get('forgetG'))
+        DIGpre=DI*cachedValues.get('inputG')*(1-cachedValues.get('inputG'))
+        DCGpre=DCG*(1-(cachedValues.get('candG')**2))
+        DOgpre=DO*cachedValues.get('outG')*(1-cachedValues.get('outG'))
         #DHCell to be passed into "previous"
         #check this if gradient is wonky
-        DCellPrevT=DCell*cachedValues.forgetG
+        DCellPrevT=DCell*cachedValues.get('forgetG')
 
-        #change in loss by changing the hidden state 
+        #change in loss by changing the hidden state
         #since hidden is affected by all weights from all the gates sum them up
-        DHPrevT=self.WeightFGHide.T@DF +self.WeightCGHide.T@DCG+self.WeightInGHide.T@DI+self.WeightOutGHide.T@DO
+        #note that use preactivation so first apply derivative of the activation functions for each gate
+        DHPrevT=self.WeightFGHide@DFGpre +self.WeightCGHide@DCGpre+self.WeightInGHide@DIGpre+self.WeightOutGHide@DOgpre
 
     # weight Gradients
     #outputGate weight grad
-        self.DwXo+=DO*cachedValues.outG*(1-cachedValues.outG)*cachedValues.x
-        self.DwHo+=DO*cachedValues.outG*(1-cachedValues.outG)*cachedValues.hOld
-        self.Dbiaso+=DO*cachedValues.outG*(1-cachedValues.outG)
+
+        
+        self.DwXo+=np.outer(cachedValues.get('x'),DO*cachedValues.get('outG')*(1-cachedValues.get('outG')))
+        self.DwHo+=np.outer(cachedValues.get('hOld'),DO*cachedValues.get('outG')*(1-cachedValues.get('outG')))
+        self.Dbiaso+=DO*cachedValues.get('outG')*(1-cachedValues.get('outG'))
 
     #candidate weight grad
-        self.DwXc+=DCG*(1-(cachedValues.candG**2))*cachedValues.x
-        self.DwHc+=DCG*(1-(cachedValues.candG**2))*cachedValues.hOld
-        self.Dbiasc+=DCG*(1-(cachedValues.candG**2))
+        self.DwXc+=np.outer(cachedValues.get('x'),DCG*(1-(cachedValues.get('candG')**2)))
+        self.DwHc+=np.outer(cachedValues.get('hOld'),DCG*(1-(cachedValues.get('candG')**2)))
+        self.Dbiasc+=DCG*(1-(cachedValues.get('candG')**2))
 
    #input weight grad
-        self.DwXi+=DI*cachedValues.inputG*(1-cachedValues.inputG)*cachedValues.x
-        self.DwHi+=DI*cachedValues.inputG*(1-cachedValues.inputG)*cachedValues.hOld
-        self.Dbiasi+=DI*cachedValues.inputG*(1-cachedValues.inputG)
+        self.DwXi+=np.outer(cachedValues.get('x'),DI*cachedValues.get('inputG')*(1-cachedValues.get('inputG')))
+        self.DwHi+=np.outer(cachedValues.get('hOld'),DI*cachedValues.get('inputG')*(1-cachedValues.get('inputG')))
+        self.Dbiasi+=DI*cachedValues.get('inputG')*(1-cachedValues.get('inputG'))
 
    #forget Gate weight grad
-        self.DwXf+=DF*cachedValues.forgetG*(1-cachedValues.forgetG)*cachedValues.x
-        self.DwHf+=DF*cachedValues.forgetG*(1-cachedValues.forgetG)*cachedValues.hOld
-        self.Dbiasf+=DF*cachedValues.forgetG*(1-cachedValues.forgetG)
+        self.DwXf+=np.outer(cachedValues.get('x'),DF*cachedValues.get('forgetG')*(1-cachedValues.get('forgetG')))
+        self.DwHf+=np.outer(cachedValues.get('hOld'),DF*cachedValues.get('forgetG')*(1-cachedValues.get('forgetG')))
+        self.Dbiasf+=DF*cachedValues.get('forgetG')*(1-cachedValues.get('forgetG'))
         return DCellPrevT,DHPrevT
 
 

Iteration4/test.py
@@ -1,60 +1,135 @@
 from LSTM import LSTMCell
 import numpy as np
 import math
-
-L=LSTMCell(2,2)
-
-L.WeightFGHide= np.array([[0.1, 0.2],
-                           [0.1, 0.2]])
+from DesnseLayer import DenseLayer
+T=3
 
 
-L.WeightFGInput = np.array([[0.1, 0.2],
-                           [0.1, 0.2]])      # shape (inputSize, hiddenSize)
-
-
-L.BiasFG = np.array([0.0, 0.0])
-
-L.WeightInGInput=np.array([[0.05, 0.10],
-                           [0.05, 0.10]])
-
-L.WeightInGHide= np.array([[0.05, 0.10],
-                           [0.05, 0.10]])
+inputsize=3
+hiddensize=3
+numclasses=5
 
 
 
-L.BiasInG = np.array([0.0, 0.0])
+X=np.random.randn(T,inputsize,)
+Y=np.random.randint(0,numclasses,T)
+
+H=np.random.randn(hiddensize,)
+C=np.random.randn(hiddensize,)
+
+originalH=H
+originalC=C
+
+L=LSTMCell(inputsize,hiddensize)
+D=DenseLayer(numclasses,hiddensize,None,'SoftMax',None)
+params_to_check = {
+    # forget gate
+    'WeightFGInput': (L.WeightFGInput, L.DwXf),
+    'WeightFGHide':  (L.WeightFGHide,  L.DwHf),
+    'BiasFG':        (L.BiasFG,        L.Dbiasf),
+
+    # input gate
+    'WeightInGInput': (L.WeightInGInput, L.DwXi),
+    'WeightInGHide':  (L.WeightInGHide,  L.DwHi),
+    'BiasInG':        (L.BiasInG,        L.Dbiasi),
+
+    # candidate gate
+    'WeightCGInput': (L.WeightCGInput, L.DwXc),
+    'WeightCGHide':  (L.WeightCGHide,  L.DwHc),
+    'BiasCG':        (L.BiasCG,        L.Dbiasc),
+
+    # output gate
+    'WeightOutGInput': (L.WeightOutGInput, L.DwXo),
+    'WeightOutGHide':  (L.WeightOutGHide,  L.DwHo),
+    'BiasOutG':        (L.BiasOutG,        L.Dbiaso),
+}
+np.random.seed(0)
+for name, (W, _) in params_to_check.items():
+    W[:] = np.random.randn(*W.shape) * 0.1
+
+cArray=[]
+predicions=[]
+for i in range(T):
+    cached=L.forward(X[i],C,H)
+    H=cached.get('hnew')
+    C=cached.get('cnew')
+    cArray.append(cached)
+    Temp=D.forward(H)
+    predicions.append(Temp)
 
 
-L.WeightCGInput = np.array([[-0.05, -0.10],
-                           [-0.05, -0.10]])
+DHPrevT=np.zeros(hiddensize)
+DCellPrevT=np.zeros(hiddensize)
+for i in reversed(range(T)):
+    DHDense=D.backward(predicions[i],Y[i])
+    DHTOT=DHDense+DHPrevT
+    DCellPrevT,DHPrevT=L.backward(cArray[i],DHTOT,DCellPrevT)
 
 
-L.WeightCGHide = np.array([[-0.05, -0.10],
-                           [-0.05, -0.10]])
+def checkMat(WeightMatrix,dwAnalatica,forwardAndLoss,H,C,epsilon=1e-5):
+
+    dwNumerical=np.zeros_like(WeightMatrix)
+
+    it=np.nditer(WeightMatrix,flags=['multi_index'])
+
+    for _ in it:
+        idx=it.multi_index
+
+        ogValue=WeightMatrix[idx]
 
 
-L.BiasCG  = np.array([0.0, 0.0])
+        WeightMatrix[idx]=ogValue+epsilon
 
-
-L.WeightOutGInput = np.array([[0.20, 0.25],
-                           [0.20, 0.25]])
-
-
-L.WeightOutGHide = np.array([[0.20, 0.25],
-                           [0.20, 0.25]])
-
-
-L.BiasOutG= np.array([0.0, 0.0])
-
-x_t     = np.array([1.0, 2.0])
-h_prev  = np.array([0.5, -0.5])
-c_prev  = np.array([0.1, 0.2])
+        lossPlus=forwardAndLoss(H,C)
 
 
 
-outH,outc=L.forward(x_t,c_prev,h_prev)
+        WeightMatrix[idx]=ogValue-epsilon
+        lossMinus=forwardAndLoss(H,C)
 
 
-print(f"hidden {outH}")
-print(f"cell {outc}")
+        #reset after changing
+        WeightMatrix[idx]=ogValue
+
+        dwNumerical[idx]=(lossPlus-lossMinus)/(2*epsilon)
+
+
+        diff=np.abs(dwAnalatica-dwNumerical)
+        denom=np.abs(dwAnalatica)+np.abs(dwNumerical)+1e-8
+
+        relErr=diff/denom
+
+    return dwNumerical,relErr
+
+
+def forwardAndLoss(H,C):
+
+    total_loss = 0
+
+    for i in range(T):
+        cached = L.forward(X[i], C, H)
+        H = cached.get('hnew')
+        C = cached.get('cnew')
+        pred = D.forward(H)                
+        total_loss += sparseCategoricalCrossEntropyLoss(pred, Y[i]) 
+
+    return total_loss 
+
+def sparseCategoricalCrossEntropyLoss(prediction,trueLabelIndex):
+
+        epsilon=1e-12
+
+        #prediction array has all the probabilities
+        # true label as index will select the probability that is computed for true label
+        # if its high loss is low
+        # if it is low loss is high (very wrong)
+        return -np.log(prediction[trueLabelIndex]+epsilon)
+    
+
+
+np.random.seed(0)
+for name, (W, dW_analytical) in params_to_check.items():
+
+    dW_numerical, relative_error = checkMat(W, dW_analytical,forwardAndLoss,originalH,originalC)
+    print(f"{name}: max relative error = {relative_error.max():.10f}")
 