6b1d781dd2696b32b949615eb87abed7cb4b71c5
hmze-git <hamzahrassul92@gmail.com>
2026-07-22 17:20:43 +0200

    chagnes

--- Pipeline1TFlow/CNNLSTM.py
+++ Pipeline1TFlow/CNNLSTM.py
@@ -1,52 +1,88 @@
 import tensorflow as tf
 from tensorflow.keras import layers,models
-import cupy as np
+from tensorflow.keras.metrics import Precision,Recall
 import math
 import time
 import pickle
+import numpy as np
 
 #following the structure by singh et. al. make the hidden size 128 as that is the size of their first dense layer
 class TFLSTMCNN:
-    def __init__(self,inputShape,hiddenSize,numClasses=3,learnRate=0.01):
+    def __init__(self,inputShape,hiddenSize,numClasses=3,learnRate=0.001):
         self.LSTMCNN=models.Sequential()
-        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(32,kernel_size=(2,2),strides=1,padding='valid',input_shape=inputShape)))
-        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(32,kernel_size=(2,2),strides=1,padding='same')))
+        self.LSTMCNN.add(layers.Input(shape=inputShape))
+        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(8,kernel_size=(3,3),strides=1,padding='valid',activation='relu')))
+        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(8,kernel_size=(3,3),strides=1,padding='same',activation='relu')))
         #add batch normalisation once batching added in
+        self.LSTMCNN.add(layers.TimeDistributed(layers.BatchNormalization()))
         self.LSTMCNN.add(layers.TimeDistributed(layers.MaxPool2D(2,2)))
 
 
-        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(64,kernel_size=(2,2),strides=1,padding='valid')))
-        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(64,kernel_size=(2,2),strides=1,padding='same')))
+        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(16,kernel_size=(3,3),strides=1,padding='valid',activation='relu')))
+        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(16,kernel_size=(3,3),strides=1,padding='same',activation='relu')))
         #add batch normalisation once batching added in
+        self.LSTMCNN.add(layers.TimeDistributed(layers.BatchNormalization()))
         self.LSTMCNN.add(layers.TimeDistributed(layers.MaxPool2D(2,2)))
 
 
-        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(128,kernel_size=(2,2),strides=1,padding='valid')))
-        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(128,kernel_size=(2,2),strides=1,padding='same')))
+        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(32,kernel_size=(3,3),strides=1,padding='valid',activation='relu')))
+        self.LSTMCNN.add(layers.TimeDistributed(layers.Conv2D(32,kernel_size=(3,3),strides=1,padding='same',activation='relu')))
         #add batch normalisation once batching added in
+        self.LSTMCNN.add(layers.TimeDistributed(layers.BatchNormalization()))
         self.LSTMCNN.add(layers.TimeDistributed(layers.MaxPool2D(2,2)))
 
-        self.LSTMCNN.add(layers.TimeDistributed(layers.Flatten()))
-        self.LSTMCNN.add(layers.TimeDistributed(layers.Dense(256,activation='relu')))
-        self.LSTMCNN.add(layers.TimeDistributed(layers.Dense(128,activation='relu')))
-        self.LSTMCNN.add(layers.LSTM(hiddenSize,))
+        #self.LSTMCNN.add(layers.TimeDistributed(layers.Flatten()))
+        
+        self.LSTMCNN.add(layers.TimeDistributed(layers.GlobalAveragePooling2D()))
+        self.LSTMCNN.add(layers.TimeDistributed(layers.Dense(64,activation='relu')))
+        self.LSTMCNN.add(layers.TimeDistributed(layers.BatchNormalization()))
+
+        self.LSTMCNN.add(layers.LSTM(hiddenSize,unroll=True))
 
 
         #final 2 dense classificaiton heads
-        self.LSTMCNN.add(layers.Dense(128,activation='relu'))
+        self.LSTMCNN.add(layers.Dense(32,activation='relu'))
         self.LSTMCNN.add(layers.Dropout(0.25))
         self.LSTMCNN.add(layers.Dense(numClasses,activation='softmax'))
 
         self.LSTMCNN.compile(
-            optimizer=tf.keras.optimizers.SGD(learning_rate=learnRate, momentum=0.9),
+            optimizer=tf.keras.optimizers.Adam(learning_rate=learnRate),
             loss='sparse_categorical_crossentropy',
             metrics=['accuracy']
         )
 
 
-    def  train(self,epochs,dataset):
+    def  train(self,epochs,dataset,validSet):
         self.LSTMCNN.fit(
             dataset,
+            validation_data=validSet,
             epochs=epochs
         )
 
+    def checkGradient(self,dataset):
+
+        for xB,yB in dataset.take(20):
+
+            with tf.GradientTape() as tape:
+                preds=self.LSTMCNN(xB,training=True)
+                loss=tf.keras.losses.sparse_categorical_crossentropy(yB,preds)
+                loss=tf.reduce_mean(loss)
+            
+            grads=tape.gradient(loss,self.LSTMCNN.trainable_variables)
+            print(f"\nLoss on this batch: {loss.numpy():.6f}\n")
+
+            # Step 3: group gradient norms by top-level layer name
+            layerGradNorms = {}
+            for var, grad in zip(self.LSTMCNN.trainable_variables, grads):
+                layerName = var.name.split('/')[0]
+                if grad is None:
+                    print(f"WARNING: {var.name} has NO gradient (None)")
+                    continue
+                gradNorm = tf.norm(grad).numpy()
+                layerGradNorms.setdefault(layerName, []).append(gradNorm)
+
+            # Step 4: print summary, in the order layers were added to the model
+            print(f"{'Layer':35s} {'Mean Grad Norm':>15s} {'Max Grad Norm':>15s} {'Min Grad Norm':>15s}")
+            print("-" * 82)
+            for layer, norms in layerGradNorms.items():
+                print(f"{layer:35s} {np.mean(norms):>15.8f} {np.max(norms):>15.8f} {np.min(norms):>15.8f}")
--- Pipeline1TFlow/test.py
+++ Pipeline1TFlow/test.py
@@ -3,19 +3,24 @@ import tensorflow as tf
 from tensorflow.keras import layers,models
 import numpy as np
 import pickle
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 xTrain=np.load('InputFiltered.npy',mmap_mode='r')
 yTrain=np.load('OutputTags.npy')
+
+xValid=np.load('validationFiltered.npy',mmap_mode='r')
+yValid=np.load('validationTags.npy')
+
 frameLengthPV=np.load('RealFrameLen.npy')
 
 #xTrain=np.asarray(xTrain)
 #yTrain=np.asarray(yTrain)
 #frameLengthPV=np.asarray(frameLengthPV)
 
-xTrain=xTrain.astype(np.float64)
-yTrain=yTrain.astype(np.int32)
 frameLengthPV=frameLengthPV.tolist()
 hiddenSize=64
 numClasses=3
+
 
 
 inShpae=xTrain.shape[1:] #remvove axis 0 from shape it only wants frame shape
@@ -27,6 +32,10 @@ def generator():
         yield xTrain[x],yTrain[x]
 
 
+def validationGenerator():
+    for x in range(len(xValid)):
+        yield xValid[x],yValid[x]
+
 dataset=tf.data.Dataset.from_generator(
     generator,
     output_signature=(
@@ -35,12 +44,43 @@ dataset=tf.data.Dataset.from_generator(
     )
   )
 
-dataset=dataset.batch(1)
+validationSet=tf.data.Dataset.from_generator(
+    validationGenerator,
+    output_signature=(
+         tf.TensorSpec(shape=xValid.shape[1:],dtype=tf.float32),
+        tf.TensorSpec(shape=(),dtype=tf.int32)
+    )
+)
 
 
-print(dataset)
+dataset = dataset.shuffle(buffer_size=1050)
+dataset = dataset.batch(batch_size=4)
+dataset = dataset.prefetch(tf.data.AUTOTUNE)
 
-LCNN.train(5,dataset)
+
+
+validationSet = validationSet.shuffle(buffer_size=225)
+validationSet = validationSet.batch(batch_size=4)
+validationSet = validationSet.prefetch(tf.data.AUTOTUNE)
+
+
+
+
+
+LCNN.train(150,dataset,validationSet)
+
+
+preds=LCNN.LSTMCNN.predict(validationSet)
+
+predLabels=np.argmax(preds,axis=1)
+trueLabels=np.concatenate([y.numpy() for x,y in validationSet],axis=0)
+
+cm=confusion_matrix(predLabels,trueLabels)
+
+disp=ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=[0,1,2])
+disp.plot(cmap='Blues')
+plt.savefig('confMatrix.png')
+plt.show()
 
 
 #with open('Save_Model_epoch_2','rb') as f:
